42c5cfb81abf2c2365321e941b1db790c61af2e8
Mr-animesh <janimesh936@gmail.com>
2025-07-10 13:43:05 +0530

    started real time chat application in React

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,52 @@
+import { generateToken } from '../lib/util.js';
+import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+
+export const signup = async (req, res) => {
+    const {fullName, email, password} = req.body;
+    try {
+        if(!fullName || !email || !password){
+            return res.status(400).json({message: "all field are required"});
+        }
+        if(password.length < 6) {
+            return res.status(400).json({message: "Password must be atleast 6 characters"});
+        }
+
+        const user = await User.findOne({email});
+        if(user) return res.status(400).json({message: "email already exists"})
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword
+        })
+
+        if(newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            })
+        }else{ 
+            res.status(400).json({message: "invalid userdata"});
+        }
+    } catch (error) {
+        console.log("error in signup controller", error.message);
+        res.status(500).json({message: "Internal Server Error"})
+    }
+};
+
+export const login = (req, res) => {
+    res.send("singup route")
+};
+
+export const logout = (req, res) => {
+    res.send("singup route")
+};
