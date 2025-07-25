2e6be739c46b9d1c76a8095d4405b24323d78e44
Mr-animesh <janimesh936@gmail.com>
2025-07-25 21:35:38 +0530

    hmmm

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const __dirname = path.resolve();   
 
 
 app.use('/api/auth', authRoutes);
@@ -26,7 +26,7 @@ app.use('/api/messages', messageRoutes);
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
     })
 }
 
