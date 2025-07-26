5fa3b1641aa77397e8b8e8206418e96b0c807704
Mr-animesh <janimesh936@gmail.com>
2025-07-27 00:17:48 +0530

    from gui

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,10 @@ import path from "path";
 import {app, server} from './lib/socket.js'
 
 dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();   
+
 app.use(express.json({ limit: "5mb" })); //helps to get json of size greater than 10kb
 app.use(cookieParser());
 app.use(cors({
@@ -16,18 +20,14 @@ app.use(cors({
     credentials: true,
 }));
 
-const PORT = process.env.PORT;
-const __dirname = path.resolve();   
-
-
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-    })
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+    });
 }
 
 server.listen(PORT, () =>{
