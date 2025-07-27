2741d8e5c4b997fc936b2b376c1d4241a50ed753
Mr-animesh <janimesh936@gmail.com>
2025-07-27 12:58:48 +0530

    checking

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,8 +10,8 @@ import {app, server} from './lib/socket.js'
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-const __dirname = path.resolve();   
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json({ limit: "5mb" })); //helps to get json of size greater than 10kb
 app.use(cookieParser());
@@ -25,12 +25,13 @@ app.use('/api/messages', messageRoutes);
 
 if(process.env.NODE_ENV === "production"){
     console.log("heelo from route from frontedn")
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
     });
 }
 
+const PORT = process.env.PORT;
 server.listen(PORT, () =>{
     console.log("Server is running on port: "+ PORT);
     connectDB();
