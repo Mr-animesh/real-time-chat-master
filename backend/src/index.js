47d0437388728a485e082e2dcf4a77eaf0c71640
Mr-animesh <janimesh936@gmail.com>
2025-07-27 13:07:29 +0530

    checking

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,7 +27,7 @@ app.use('/api/messages', messageRoutes);
 if(process.env.NODE_ENV === "production"){
     console.log("heelo from route from frontedn")
     app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
     });
 }
